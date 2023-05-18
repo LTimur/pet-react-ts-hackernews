3d3d4100c2b0b0093b9de07cd5f831ff12320ff6
LTimur <ledenev-timur@ya.ru>
2023-05-18 16:44:43 +0500

    refactor HackernewsApi

--- src/services/HackernewsApi.tsx
+++ src/services/HackernewsApi.tsx
@@ -4,7 +4,7 @@ export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`
 
-export const getStory = async (storyId) => {
+export const getStory = async (storyId: string) => {
   try {
     const response = await fetch(`${storyUrl + storyId}.json`);
     if (response.ok) {
